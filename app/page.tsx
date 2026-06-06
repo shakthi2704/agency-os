91b30663ba58cb60780074c74c146b53c2b661fb
shakthi <shakthi2704@gmail.com>
2026-06-06 11:51:06 +0530

    feat: dependencies installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div>
       <h1>Agency OS</h1>
+      <Button variant="outline" size="icon" aria-label="Submit">
+        test
+      </Button>
     </div>
   );
 }
